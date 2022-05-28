29ff0c93d6c49435cf89607dbe5b3ee2f5092f11
Ekaterina <Kuteynikova.ekaterina@mail.ru>
2022-05-29 00:18:41 +0300

    Home_02

---
+++ Home_02/Program.cs
@@ -0,0 +1,33 @@
+﻿void Print(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNum(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-100, 100);
+    }
+    return arr;
+}
+
+void SumIndexNum(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 1; i < arr.Length; i= i+2) sum = sum + arr[i];
+    
+    Console.WriteLine(sum);
+
+}
+
+int[] arr_1 = MassNum(12);
+Print(arr_1);
+SumIndexNum(arr_1);
